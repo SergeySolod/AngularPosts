88fd89a2d7fe01928db49112468d0107fc558d71
SergeySolod <solodsergey287@gmail.com>
2020-03-14 14:19:21 +0300

    Change image in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dynamic title'
-  number = 42
-  arr = [1, 2, 3]
-  obj = { a: 1, b: {c: 2}}
+    img = 'https://ru.reactjs.org/logo-og.png'
+    constructor() {
+        setTimeout(() => {
+            this.img = 'https://www.searchengines.ru/wp-content/uploads/2019/02/756150_c033_2.jpg'
+        }, 5000)
+    }
 }
