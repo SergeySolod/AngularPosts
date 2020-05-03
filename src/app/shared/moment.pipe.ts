db2a807a8e54c7ef0eb99491f7032257f6cafb4f
SergeySolod <solodsergey287@gmail.com>
2020-05-03 23:48:42 +0300

    Add moment pipe

--- src/app/shared/moment.pipe.ts
+++ src/app/shared/moment.pipe.ts
@@ -1,5 +1,13 @@
-import {Pipe} from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
     name: 'moment'
 })
+
+export class MomentPipe implements PipeTransform{
+    transform(m: moment.Moment, format: string = 'MMM YYYY'): string {
+        return m.format(format)
+    }
+
+}
