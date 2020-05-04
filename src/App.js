78bc0f33ccfb122c76db0dd0b633c710d701bc66
SergeySolod <solodsergey287@gmail.com>
2020-05-04 16:46:17 +0300

    Api modifiedData

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { fetchData }  from './api'
 class App extends React.Component {
     async componentDidMount () {
         const data = await fetchData()
+        console.log(data)
     }
     render() {
         return (
