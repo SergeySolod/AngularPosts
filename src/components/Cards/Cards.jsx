cab2fe5d9c28a43f53304e783f2ac71b4bfc5fb5
SergeySolod <solodsergey287@gmail.com>
2020-05-04 20:03:18 +0300

    Before test

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import styles from './Card.module.css'
 
 const Cards = (props) => {
     return (
-        <h1>
-            Cards
-        </h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify='center'>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography variant='body2'>Number of active cases</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
