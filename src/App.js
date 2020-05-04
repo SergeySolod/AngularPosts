5893865572c7fc9d421ab6bd0641ff80796b50da
SergeySolod <solodsergey287@gmail.com>
2020-05-04 17:31:08 +0300

    State im app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import styles from './App.module.css'
 import { fetchData }  from './api'
 
 class App extends React.Component {
+    state = {
+        data: {},
+    }
+
     async componentDidMount () {
-        const data = await fetchData()
-        console.log(data)
+        const fetchData = await fetchData()
+        this.setState({ data: fetchData })
     }
     render() {
+        const {data} = this.state;
         return (
             <div className={styles.container}>
-                <Cards/>
+                <Cards data={data}/>
                 <Chart/>
                 <CountryPicker/>
             </div>
