e09120618d0d094162e712017f2782abe82b7374
SergeySolod <solodsergey287@gmail.com>
2020-05-06 01:23:03 +0300

    Add image COVID-19

--- src/pages/CovidPage.js
+++ src/pages/CovidPage.js
@@ -1,22 +1,31 @@
 import React, {useEffect} from 'react'
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import {SetCovidDataThunk} from "../redux/reducers/Covid-reducer";
+import CovidCard from "./CovidCard";
 
 const CovidPage = (props) => {
     useEffect(() => {
         props.SetCovidDataThunk()
-        }, []);
+    }, []);
     return (
         <div className='container'>
-
+            <div className='row'>
+                <div className='col s12 m4'>
+                    <CovidCard/>
+                </div>
+                <div className='col s12 m4'>
+                    <CovidCard/>
+                </div>
+                <div className='col s12 m4'>
+                    <CovidCard/>
+                </div>
+            </div>
         </div>
     )
 }
 
 const mapStateToProps = state => {
-    return {
-
-    };
+    return {};
 };
 
 export default connect(mapStateToProps, {SetCovidDataThunk})(CovidPage)
