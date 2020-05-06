5c496d7c6aa19f089387dd9fae3ea34361e0b476
SergeySolod <solodsergey287@gmail.com>
2020-05-07 01:32:35 +0300

    Before grid material ui

--- src/pages/Dropdown.js
+++ src/pages/Dropdown.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import Grid from '@material-ui/core/Grid';
 
 const Dropdown = (props) => {
     return (
-        <Autocomplete
-            id="combo-box-demo"
-            options={props.Countries}
-            getOptionLabel={(option) => option.country}
-            style={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-        />
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+        >
+            <Grid item xs={3}>
+                <Autocomplete
+                    id="combo-box-demo"
+                    options={props.Countries}
+                    getOptionLabel={(option) => option.country}
+                    style={{width: 300}}
+                    renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined"/>}
+                />
+            </Grid>
+        </Grid>
     );
 }
 
