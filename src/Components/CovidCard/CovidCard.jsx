236bbb7a56218825544cd98671db5010280e9135
SergeySolod <solodsergey287@gmail.com>
2020-05-07 13:24:48 +0300

    Card component without Grid container

--- src/Components/CovidCard/CovidCard.jsx
+++ src/Components/CovidCard/CovidCard.jsx
@@ -5,8 +5,6 @@ import styles from './CovidCard.module.css'
 
 const CovidCard = (props) => {
     return (
-        <div className={styles.container}>
-            <Grid container spacing={3} justify='center'>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
@@ -23,40 +21,6 @@ const CovidCard = (props) => {
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                            Recovered
-                        </Typography>
-                        <Typography variant="h5">
-                            REAL DATE
-                        </Typography>
-                        <Typography color="textSecondary">
-                            REAL DATE
-                        </Typography>
-                        <Typography variant="body2">
-                            Number of recoveries from COVID-19
-                        </Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                            Deaths
-                        </Typography>
-                        <Typography variant="h5">
-                            REAL DATE
-                        </Typography>
-                        <Typography color="textSecondary">
-                            REAL DATE
-                        </Typography>
-                        <Typography variant="body2">
-                            Number of deaths caused by COVID-19
-                        </Typography>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        </div>
     )
 }
 
--- src/pages/CovidPage.js
+++ src/pages/CovidPage.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import styles from './CovidPage.module.css';
 import CovidCard from "../Components/CovidCard/CovidCard";
+import {Grid} from '@material-ui/core'
 
 const CovidPage = () => {
     return (
-     <div className={styles.container}>
-       <CovidCard/>
-     </div>
+        <div className={styles.container}>
+            <div className={styles.container_card}>
+                <Grid container spacing={3} justify='center'>
+                    <CovidCard/>
+                </Grid>
+            </div>
+        </div>
     )
 }
 
 
 export default (CovidPage)
 
-{/*<Grid container spacing={2} justify="center">*/}
-{/*<Grid item xs={12} md={3} className={styles.card}>*/}
-{/*<div className='covid-logo'><img src={CovidLogo} alt="Covid logo"/></div>*/}
-{/*<div>*/}
-{/*{props.CovidData.map(data => <div className='col s12 m4'><CovidCard data={data}/></div>)}*/}
-{/*/!*<Dropdown Countries={props.Countries}/>*!/*/}
-{/*/!*<CovidChart data={props.CovidEveryDayData}/>*!/*/}
-{/*</div>*/}
-{/*</Grid>*/}
-{/*</Grid>*/}
+{/*<Grid container spacing={2} justify="center">*/
+}
+{/*<Grid item xs={12} md={3} className={styles.card}>*/
+}
+{/*<div className='covid-logo'><img src={CovidLogo} alt="Covid logo"/></div>*/
+}
+{/*<div>*/
+}
+{/*{props.CovidData.map(data => <div className='col s12 m4'><CovidCard data={data}/></div>)}*/
+}
+{/*/!*<Dropdown Countries={props.Countries}/>*!/*/
+}
+{/*/!*<CovidChart data={props.CovidEveryDayData}/>*!/*/
+}
+{/*</div>*/
+}
+{/*</Grid>*/
+}
+{/*</Grid>*/
+}
